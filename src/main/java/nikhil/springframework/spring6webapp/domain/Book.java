1b441b01bdbdfca641380e693b3fa8d8306a4655
vs code <vscode@Nikhils-MacBook-Air.local>
2024-03-15 15:39:16 +0530

    subsequent commit

--- src/main/java/nikhil/springframework/spring6webapp/domain/Book.java
+++ src/main/java/nikhil/springframework/spring6webapp/domain/Book.java
@@ -2,6 +2,7 @@ package nikhil.springframework.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -9,6 +10,7 @@ public class Book{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
 
@@ -40,4 +42,36 @@ public class Book{
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", Authors=" + Authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
+
+
+
+
+
+
+
+
+
